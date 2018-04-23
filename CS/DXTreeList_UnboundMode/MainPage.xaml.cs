d0f74a50309a6cb24b7a85853d68f687fdfa3b0b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXTreeList_UnboundMode/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Controls;
+using DevExpress.Xpf.Grid;
+
+namespace DXTreeList_UnboundMode {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            BuildTree();
+            treeListView1.ExpandAllNodes();
+        }
+
+        private void BuildTree() {
+            TreeListNode rootNode = CreateRootNode(new ProjectObject() { ProjectName = "Project: Stanton", Executor = "Nicholas Llams" });
+            TreeListNode childNode = CreateChildNode(rootNode, new StageObject() { ProjectName = "Information Gathering", Executor = "Ankie Galva" });
+            CreateChildNode(childNode, new TaskObject() { ProjectName = "Design", Executor = "Reardon Felton", State = "In progress" });
+        }
+
+        private TreeListNode CreateRootNode(object dataObject) {
+            TreeListNode rootNode = new TreeListNode(dataObject);
+            treeListView1.Nodes.Add(rootNode);
+            return rootNode;
+        }
+
+        private TreeListNode CreateChildNode(TreeListNode parentNode, object dataObject) {
+            TreeListNode childNode = new TreeListNode(dataObject);
+            parentNode.Nodes.Add(childNode);
+            return childNode;
+        }
+    }
+
+    public class StageObject {
+        public String ProjectName { get; set; }
+        public string Executor { get; set; }
+    }
+
+    public class ProjectObject {
+        public String ProjectName { get; set; }
+        public string Executor { get; set; }
+    }
+
+    public class TaskObject {
+        public String ProjectName { get; set; }
+        public string Executor { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ VB/DXTreeList_UnboundMode/MainPage.xaml.vb
@@ -0,0 +1,105 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Grid
+
+Namespace DXTreeList_UnboundMode
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			BuildTree()
+			treeListView1.ExpandAllNodes()
+		End Sub
+
+		Private Sub BuildTree()
+			Dim rootNode As TreeListNode = CreateRootNode(New ProjectObject() With {.ProjectName = "Project: Stanton", .Executor = "Nicholas Llams"})
+			Dim childNode As TreeListNode = CreateChildNode(rootNode, New StageObject() With {.ProjectName = "Information Gathering", .Executor = "Ankie Galva"})
+			CreateChildNode(childNode, New TaskObject() With {.ProjectName = "Design", .Executor = "Reardon Felton", .State = "In progress"})
+		End Sub
+
+		Private Function CreateRootNode(ByVal dataObject As Object) As TreeListNode
+			Dim rootNode As New TreeListNode(dataObject)
+			treeListView1.Nodes.Add(rootNode)
+			Return rootNode
+		End Function
+
+		Private Function CreateChildNode(ByVal parentNode As TreeListNode, ByVal dataObject As Object) As TreeListNode
+			Dim childNode As New TreeListNode(dataObject)
+			parentNode.Nodes.Add(childNode)
+			Return childNode
+		End Function
+	End Class
+
+	Public Class StageObject
+		Private privateProjectName As String
+		Public Property ProjectName() As String
+			Get
+				Return privateProjectName
+			End Get
+			Set(ByVal value As String)
+				privateProjectName = value
+			End Set
+		End Property
+		Private privateExecutor As String
+		Public Property Executor() As String
+			Get
+				Return privateExecutor
+			End Get
+			Set(ByVal value As String)
+				privateExecutor = value
+			End Set
+		End Property
+	End Class
+
+	Public Class ProjectObject
+		Private privateProjectName As String
+		Public Property ProjectName() As String
+			Get
+				Return privateProjectName
+			End Get
+			Set(ByVal value As String)
+				privateProjectName = value
+			End Set
+		End Property
+		Private privateExecutor As String
+		Public Property Executor() As String
+			Get
+				Return privateExecutor
+			End Get
+			Set(ByVal value As String)
+				privateExecutor = value
+			End Set
+		End Property
+	End Class
+
+	Public Class TaskObject
+		Private privateProjectName As String
+		Public Property ProjectName() As String
+			Get
+				Return privateProjectName
+			End Get
+			Set(ByVal value As String)
+				privateProjectName = value
+			End Set
+		End Property
+		Private privateExecutor As String
+		Public Property Executor() As String
+			Get
+				Return privateExecutor
+			End Get
+			Set(ByVal value As String)
+				privateExecutor = value
+			End Set
+		End Property
+		Private privateState As String
+		Public Property State() As String
+			Get
+				Return privateState
+			End Get
+			Set(ByVal value As String)
+				privateState = value
+			End Set
+		End Property
+	End Class
+End Namespace
